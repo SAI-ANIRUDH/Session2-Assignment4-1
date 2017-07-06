03883b2769cfe5c07387ef3b7a1001dcf9c7f15c
SAI-ANIRUDH <anirudh@saisam>
2017-07-06 10:39:56 +0530

    Session2-Assignment4.1

---
+++ src/TestDemo.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class TestDemo {
+
+	public static void main(String[] abc) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		char x=4;
+		int ascii=(char)x;
+			
+			abc((char)x);
+				}
+			public static void abc(int x) { 
+
+				 //x = 0; 
+				 if (x==0)
+				 {
+					System.out.println("You Have Entered Zero x"+(char)x);
+				}
+				else if (x>0) {
+					System.out.println("You Have Positive number" +(char)x);
+				}
+				else if(x<0) {
+					System.out.println("You Have Negative Number"+(char)x);
+				}
+			}
+			 }
+		
