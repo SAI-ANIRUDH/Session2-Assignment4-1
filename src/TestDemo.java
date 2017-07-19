132fdbb36e3d32ae794a20e70381b95e656d69dc
SAI-ANIRUDH <SAI-ANIRUDH@users.noreply.github.com>
2017-07-19 11:52:48 +0530

    Update TestDemo.java

--- src/TestDemo.java
+++ src/TestDemo.java
@@ -1,23 +1,25 @@
-package assignment2;
-import java.util.Scanner;
+package assignment2; // Declaring package //
+import java.util.Scanner; // Importing Scaner class from Util Package //
 public class TestDemo {
 
 	public static void main(String[] abc) {
 		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		char x=4;
-		int ascii=(char)x;
+		Scanner sc=new Scanner(System.in); // Taking input from user //
+		char x=4; // Declaring variable x //
+		int ascii=(char)x; // Type Casting char variable into int value //
 			
 			abc((char)x);
 				}
-			public static void abc(int x) { 
+			public static void abc(int x) // Declaring and initializing method abc //
+			{ 
 
 				 //x = 0; 
 				 if (x==0)
 				 {
 					System.out.println("You Have Entered Zero x"+(char)x);
 				}
-				else if (x>0) {
+				else if (x>0)
+				{
 					System.out.println("You Have Positive number" +(char)x);
 				}
 				else if(x<0) {
